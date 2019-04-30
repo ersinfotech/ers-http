b2da6f78a3a91016a44b4371d652010e0d34e764
jarvisaoieong <kinua1230@gmail.com>
2019-04-30 23:09:17 +0800

    use request and bump 5.5

--- restrict.js
+++ restrict.js
@@ -1,48 +1,46 @@
-
-const request = require('superagent')
+const request = require('request-promise')
 
 module.exports = (options = {}) => {
+  const { baseUrl } = options
 
-    const {
-        baseUrl,
-    } = options
+  if (!baseUrl) throw new Error('baseUrl required in restrict middleware')
 
-    if (!baseUrl) throw new Error('baseUrl required in restrict middleware')
+  return () => (req, res, next) => {
+    const error = message => next(new Error(message))
 
-    return () => (req, res, next) => {
+    let query = req.query || {}
 
-        const error = (message) => next(new Error(message))
+    if (req.handshake) {
+      query = req.handshake.query
+      next = res
+    }
 
-        let query = req.query || {}
+    const access_token = query.access_token
 
-        if (req.handshake) {
-            query = req.handshake.query
-            next = res
+    if (!access_token) return error('access_token required')
+
+    request({
+      url: baseUrl + '/account/me/id',
+      qs: { access_token },
+      json: true,
+    })
+      .then(res => {
+        if (res.error) return error('access_token invalid')
+
+        const data = res.body || {}
+
+        req.session = {
+          ...req.session,
+          userId: String(data.user_id),
+          groupId: String(data.group_id),
+          productId: String(data.product_id),
+          roleId: String(data.role_id),
+          lang: data.lang,
+          accessToken: access_token,
         }
 
-        const access_token = query.access_token
-
-        if (!access_token) return error('access_token required')
-
-        request.get(baseUrl + '/account/me/id')
-            .query({access_token})
-            .then(res => {
-                if (res.error) return error('access_token invalid')
-
-                const data = res.body || {}
-
-                req.session = {
-                    ...req.session,
-                    userId: String(data.user_id),
-                    groupId: String(data.group_id),
-                    productId: String(data.product_id),
-                    roleId: String(data.role_id),
-                    lang: data.lang,
-                    accessToken: access_token
-                }
-
-                next()
-            })
-            .catch(e => error('access_token fetching error'))
-    }
+        next()
+      })
+      .catch(e => error('access_token fetching error'))
+  }
 }
