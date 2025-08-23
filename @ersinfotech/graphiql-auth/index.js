f506c70c0c024a6f1e06df45e3a8397bceecbe52
odootest2023 <odootest2023@163.com>
2025-08-23 13:53:01 +0800

    format code

--- @ersinfotech/graphiql-auth/index.js
+++ @ersinfotech/graphiql-auth/index.js
@@ -1,15 +1,11 @@
 const jsonreq = require('../../jsonreq')
 
-module.exports = ({
-  eadminBaseUrl,
-  clientId,
-  callbackUrl,
-}) => ({
+module.exports = ({ eadminBaseUrl, clientId, callbackUrl }) => ({
   get: (req, res) => {
     res.sendFile(__dirname + '/login.html')
   },
   post: (req, res, next) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
     jsonreq({
       url: eadminBaseUrl + '/oauth/signin',
       json: {
@@ -18,15 +14,15 @@ module.exports = ({
         password,
       },
     })
-    .then(({access_token}) => {
-      res.cookie('ers-http', Date.now(), {
-        httpOnly: true,
-        sameSite: true,
+      .then(({ access_token }) => {
+        res.cookie('ers-http', Date.now(), {
+          httpOnly: true,
+          sameSite: true,
+        })
+        res.redirect(callbackUrl + `?access_token=${access_token}`)
       })
-      res.redirect(callbackUrl + `?access_token=${access_token}`)
-    })
-    .catch(() => {
-      next(new Error('login fail'))
-    })
-  }
+      .catch(() => {
+        next(new Error('login fail'))
+      })
+  },
 })
--- graphql.js
+++ graphql.js
@@ -1,32 +1,38 @@
-
-const {graphiql} = require('graphql-ready')
+const { graphiql } = require('graphql-ready')
 const GraphiqlAuth = require('./@ersinfotech/graphiql-auth')
 
-module.exports = (app, config, {schema, api, resolver, gql = []}, restrict) => {
-
-    const graphLogin = (callbackUrl) => GraphiqlAuth({
+module.exports = (
+  app,
+  config,
+  { schema, api, resolver, gql = [] },
+  restrict,
+) => {
+  const graphLogin = (callbackUrl) =>
+    GraphiqlAuth({
       eadminBaseUrl: config.eadmin.baseUrl,
       clientId: config.clientId,
       callbackUrl,
     })
 
-    const graphiqlGetNotFound = (req, res, next) => {
-      const re = new RegExp(`${req.baseUrl}/login$`)
-      if (req.method === 'GET' && !re.test(req.headers.referer)) {
-        return res.status(404).end('Not Found')
-      } else if (req.method === 'POST' && /__schema/.test(req.body.query) && !/ers-http=\d+/.test(req.headers['x-ers-http'] || req.headers.cookie)) {
-        return res.status(400).end('Not Found')
-      }
-      next()
+  const graphiqlGetNotFound = (req, res, next) => {
+    const re = new RegExp(`${req.baseUrl}/login$`)
+    if (req.method === 'GET' && !re.test(req.headers.referer)) {
+      return res.status(404).end('Not Found')
+    } else if (
+      req.method === 'POST' &&
+      /__schema/.test(req.body.query) &&
+      !/ers-http=\d+/.test(req.headers['x-ers-http'] || req.headers.cookie)
+    ) {
+      return res.status(400).end('Not Found')
     }
+    next()
+  }
 
-    const graphin = graphLogin('../graphql')
+  const graphin = graphLogin('../graphql')
 
-    app.route('/login')
-    .get(graphin.get)
-    .post(graphin.post)
+  app.route('/login').get(graphin.get).post(graphin.post)
 
-    const graphql = graphiql({schema, api, resolver})
+  const graphql = graphiql({ schema, api, resolver })
 
-    app.use('/', graphiqlGetNotFound, restrict(), gql, graphql)
+  app.use('/', graphiqlGetNotFound, restrict(), gql, graphql)
 }
